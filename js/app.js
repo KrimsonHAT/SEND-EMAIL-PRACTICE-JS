695f0dead90d7c8e43015a5dcad54d4870023644
KrimsonHAT <fabtroydsounds@gmail.com>
2020-11-12 18:08:42 -0600

    JS PRACTICE PROJECT SEND EMAIL ENDING

---
+++ js/app.js
@@ -0,0 +1,150 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+const resetBtn = document.getElementById('resetBtn');
+const formulario = document.querySelector('#enviar-mail');
+
+
+
+
+// variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje= document.querySelector('#mensaje');
+
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+       
+
+eventListenners();
+
+function eventListenners(){
+    // La app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+
+    // campos del formulario
+    email.addEventListener('blur',validarFormulario);
+    asunto.addEventListener('blur',validarFormulario);
+   mensaje.addEventListener('blur',validarFormulario);
+
+    
+
+//    Enviar email
+formulario.addEventListener('submit',enviarEmail);
+
+// Restea el formulario
+resetBtn.addEventListener('click',resetearFormulario);
+}
+
+
+
+
+
+
+
+
+
+
+// funciones
+function iniciarApp(){
+    // console.log("iniciciando app");
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
+};
+
+// validar formulario
+function validarFormulario(e) { 
+
+    
+    if(e.target.value.length > 0 ){
+    //    console.log('si hay algo');
+    // elimina los errores
+    const error = document.querySelector('p.error');
+    if(error){
+        error.remove();
+    }
+   
+
+    e.target.classList.remove('border','border-red-500');
+    e.target.classList.add('border','border-green-500');
+   
+    }else{
+        e.target.classList.remove('border','border-green-500');
+        e.target.classList.add('border','border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+
+    if(e.target.type === 'email'){
+       
+        if(er.test(e.target.value)){
+            // elimina los errores
+             const error = document.querySelector('p.error');
+             if(error){
+                error.remove();
+            }
+           
+            
+            e.target.classList.remove('border','border-red-500');
+            e.target.classList.add('border','border-green-500');
+        }else{
+            e.target.classList.remove('border','border-green-500');
+            e.target.classList.add('border','border-red-500');
+              mostrarError('Email no válido');
+        }
+    }
+   
+  
+    if(er.test( email.value ) && asunto.value !== '' && mensaje.value !== ''){
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50')
+    }
+}    
+ 
+
+ function mostrarError(mensaje){
+     const mensajeError = document.createElement('p');
+     mensajeError.textContent = mensaje;
+     mensajeError.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-5', 'text-center','error');
+    
+    const errores = document.querySelectorAll('.error');
+
+     if(errores.length ===0){
+
+        formulario.appendChild(mensajeError);
+    }
+     
+ }
+
+//  enviar email
+function enviarEmail(e){
+    e.preventDefault();
+    // MOstrar spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'flex';
+   
+   
+   // Despues de 3seg ocultar el spinner y mostrar el mnsaje
+    setTimeout( () => {
+        spinner.style.display = 'none';
+        // 
+        const parrafo = document.createElement('p');
+        parrafo.textContent = ' El mensaje se envío correctamente';
+        parrafo.classList.add('text-center','my-10','p-2','bg-green-500','text-white','font-bold','uppercase')
+        // inserta el parrafo antes del spinner
+        formulario.insertBefore(parrafo,spinner);
+
+        setTimeout( () => {
+            parrafo.remove();
+            resetearFormulario();
+            
+        },5000);
+    },3000);
+}
+
+// funcion resetea el formulario
+
+function resetearFormulario(e) { 
+    e.preventDefault();
+    formulario.reset();
+    iniciarApp();
+ }
